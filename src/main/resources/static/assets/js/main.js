2f4e3c1d6da5bdf92c25229b1da8d74ac3c82d92
Hyorim-Kim <hyorim.kim@students.mq.edu.au>
2023-09-25 14:56:02 +0900

    hyorim commit 0925

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -1,15 +1,11 @@
-/**
-* Template Name: UpConstruction - v1.3.0
-* Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+ /*DOMContentLoaded 이벤트를 기다리고, 페이지가 로드되면 다양한 작업을 수행*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
-  /**
-   * Preloader
-   */
+  /*Preloader : 페이지가 로딩 중일 때 사용자에게 로딩 중임을 시각적으로 알려주는 기능*/
+  /* #preloader라는 ID를 가진 요소를 찾고 해당 요소가 존재하면,
+  페이지가 로드될 때 window의 load 이벤트를 기다렸다가 preloader 요소를 제거한다.
+  결과적으로 페이지가 완전히 로드되면 preloader가 화면에서 사라지게 된다.*/ 
   const preloader = document.querySelector('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
@@ -17,10 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  /**
-   * Mobile nav toggle
-   */
-
+  /*모바일 네비게이션 토글(햄버거 메뉴바) : 모바일 네비게이션을 열고 닫기 위한 버튼을 처리한다.
+  mobileNavShow와 mobileNavHide 요소를 찾는다.
+  .mobile-nav-toggle 클래스를 가진 모든 요소에 대해 클릭 이벤트를 추가하고,
+  클릭 시 mobileNavToggle() 함수를 호출하여 네비게이션의 표시 여부를 토글한다.*/
   const mobileNavShow = document.querySelector('.mobile-nav-show');
   const mobileNavHide = document.querySelector('.mobile-nav-hide');
 
@@ -37,9 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
     mobileNavHide.classList.toggle('d-none');
   }
 
-  /**
-   * Hide mobile nav on same-page/hash links
-   */
+  /*같은 페이지/해시 링크를 클릭할 때 모바일 네비게이션 숨기기:
+  #navbar 내부의 모든 링크에 대해 반복한다.
+  만약 링크가 해시(페이지 내부 링크)를 가지고 있다면, 해당 해시로 이동하는 클릭 이벤트에 대한 처리를 추가한다.
+  모바일 네비게이션이 활성화되어 있다면, 링크를 클릭하면 mobileNavToggle() 함수를 호출하여 모바일 네비게이션을 닫는다.*/
   document.querySelectorAll('#navbar a').forEach(navbarlink => {
 
     if (!navbarlink.hash) return;
@@ -55,9 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   });
 
-  /**
-   * Toggle mobile nav dropdowns
-   */
+  /*모바일 네비게이션 드롭다운(햄버거 메뉴바 하위메뉴 펼치는거) 토글: 네비게이션 바 내부의 드롭다운 메뉴를 처리
+  .navbar .dropdown > a 요소를 찾습니다.
+  모바일 네비게이션이 활성화되어 있을 때, 드롭다운 메뉴를 클릭하면 해당 메뉴가 확장되고 축소되며, 아이콘도 변경된다.*/
   const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
 
   navDropdowns.forEach(el => {
@@ -74,9 +71,9 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   });
 
-  /**
-   * Scroll top button
-   */
+  /*스크롤 탑 버튼: 페이지의 상단으로 스크롤하는 버튼을 처리
+  스크롤 이벤트를 사용하여 페이지가 일정 위치 이상으로 스크롤되면 버튼이 나타나도록 한다.
+  버튼을 클릭하면 페이지가 맨 위로 스크롤된다.*/
   const scrollTop = document.querySelector('.scroll-top');
   if (scrollTop) {
     const togglescrollTop = function() {
@@ -90,16 +87,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
   }
 
-  /**
-   * Initiate glightbox
-   */
+  /*glightbox 초기화: glightbox 라이브러리를 사용하여 이미지 및 비디오 라이트박스를 초기화
+  .glightbox 클래스를 가진 요소들을 라이트박스로 만든다.*/
   const glightbox = GLightbox({
     selector: '.glightbox'
   });
 
-  /**
-   * Porfolio isotope and filter
-   */
+  /*포트폴리오 필터 및 정렬: 포트폴리오 아이템을 필터링하고 정렬하는 기능을 추가
+  Isotope 라이브러리를 사용하여 포트폴리오 항목을 필터링하고 정렬한다.*/
   let portfolionIsotope = document.querySelector('.portfolio-isotope');
 
   if (portfolionIsotope) {
@@ -134,9 +129,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
 
-  /**
-   * Init swiper slider with 1 slide at once in desktop view
-   */
+  /*숫자 카운터 초기화: 숫자 카운터를 초기화하는 코드 
+  웹 페이지에서 특정 횟수나 수치를 애니메이션 효과와 함께 보여주기 위한 것*/
   new Swiper('.slides-1', {
     speed: 600,
     loop: true,
@@ -156,9 +150,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  /**
-   * Init swiper slider with 2 slides at once in desktop view
-   */
+  /*스크롤 시 애니메이션 초기화:
+  AOS.init() 함수를 사용하여 스크롤 시 나타나는 애니메이션을 초기화
+  load 이벤트가 발생할 때 초기화됨*/
   new Swiper('.slides-2', {
     speed: 600,
     loop: true,
@@ -188,11 +182,13 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   });
-
-  /**
-   * Initiate pURE cOUNTER
-   */
-  new PureCounter();
+  
+  /*애니메이션 라이브러리 초기화*/
+  
+  /*Pure Counter 초기화:
+  Pure Counter는 숫자 카운터를 웹 페이지에 추가하는 데 사용되는 라이브러리
+  숫자 카운터는 웹 페이지에서 숫자를 부드럽게 증가시키거나 감소시키는데 사용된다.*/
+  new PureCounter(); /*Pure Counter를 초기화하고 페이지 내에서 숫자 카운터를 사용할 수 있도록 설정*/
 
   /**
    * Animation on scroll function and init
