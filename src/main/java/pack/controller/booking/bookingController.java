83fa073969e0e1070361722056b74e5fba9c5ff9
gwon0919 <gwon96@gmail.com>
2023-09-26 17:19:23 +0900

    jiwon bookingInfo 수정 230926

--- src/main/java/pack/controller/booking/bookingController.java
+++ src/main/java/pack/controller/booking/bookingController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pack.model.booking.bookingDTO;
+import pack.model.user.UserDto;
 import pack.controller.FormBean;
 import pack.model.booking.BookingDao;
 import pack.model.booking.BookingMapperInter;
@@ -35,21 +36,21 @@ public class bookingController {
 	}
 	//예약하기
 	@PostMapping("bookingDo")
-	public String bookingDo() {
-		 
+	public String bookingDo(bookingDTO bookingdto) {
+		boolean b = dao.bookingInsert(bookingdto);
 		return "booking/bookingInfo";
 	}
-
+	
+	 
 	
 
 	@GetMapping("bookingList")
 	public String bookingCheck(HttpSession session, Model model){
-		String user_id = (String)session.getAttribute("user_id");
-		ArrayList<bookingDTO> blist =(ArrayList<bookingDTO>)dao.bookingAll();
+		bookingDTO book = (bookingDTO)session.getAttribute("user_id");
 		session.setAttribute("booking", blist);
-		model.addAttribute("bklist", blist);
+		model.addAttribute("list", blist);
 		
-		return "/bookingInfo";
+		return "bookingInfo";
 	}
 
 
--- src/main/java/pack/model/booking/BookingDao.java
+++ src/main/java/pack/model/booking/BookingDao.java
@@ -7,26 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import pack.controller.FormBean;
 import pack.model.booking.bookingDTO;
+import pack.model.user.UserDto;
 
 @Repository
 public class BookingDao {
 	@Autowired
 	private BookingMapperInter bookingMapperInter;
-	
+
 	// 예약자료 읽기
-	public ArrayList<bookingDTO> bookingAll(){
-		ArrayList<bookingDTO> blist = (ArrayList<bookingDTO>)bookingMapperInter.bookingList("user_id");
+	public ArrayList<bookingDTO> bookingAll() {
+		ArrayList<bookingDTO> blist = (ArrayList<bookingDTO>) bookingMapperInter.bookingList("user_id");
 		return blist;
 	}
 	
 	@Transactional
-	public boolean bookingDo(FormBean bean) {
+	public boolean bookingInsert(bookingDTO bookingdto) {
 		boolean b = false;
-		String re = bookingMapperInter.bookingDo(bean);
-		
+		try {
+			int re = bookingMapperInter.bookingInsert(bookingdto);
+			if (re > 0) {
+				b = true;
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		return b;
 	}
-}
 
+	
+}
