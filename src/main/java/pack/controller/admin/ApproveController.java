7f7ff59873a96c2fa5ffd8c13cabe9e71fa321d4
mhchoi <mhchoi7443@gmail.com>
2023-09-27 15:56:17 +0900

    9/27 창고승인 기능 구현 / 세부기능 조정 필요

--- src/main/java/pack/controller/admin/ApproveController.java
+++ src/main/java/pack/controller/admin/ApproveController.java
@@ -4,26 +4,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pack.model.DataDao;
+import pack.model.admin.AdminBean;
+import pack.model.admin.AdminDao;
 import pack.model.container.ContainerDto;
 
-@RequestMapping("approve")
+
 @Controller
 public class ApproveController {
 	@Autowired
 	private DataDao dataDao;
+	@Autowired
+	private AdminDao adminDao;
 	
-	@GetMapping("detail")
+	@GetMapping("/detail")
 	public String detail(@RequestParam("cont_no")String cont_no, Model model) {
-		ContainerDto containerDto = dataDao.condetail(cont_no);
+		ContainerDto containerDto = dataDao.condetail(cont_no); 
 		model.addAttribute("containerDto",containerDto);
-		return "admin/cont_approve";
+		return "../templates/admin/cont_approve";
 	}
-	@GetMapping("/process")
-	public String goProcess() {
-		return "cont_approve";
+	
+	
+	@PostMapping("/apprProcess")
+	public String approveProcess(AdminBean bean) {
+		System.out.println("bean" + bean); 
+		boolean b = adminDao.approve(bean);
+		 System.out.println("b" + b); 
+		if (b)
+			return "redirect:/registered"; // 수정 후 목록보기
+		else
+			return "error";
+	}
+	
+	@PostMapping("/denyProcess")
+	public String denyProcess(AdminBean bean) {
+		System.out.println("bean" + bean); 
+		boolean b = adminDao.deny(bean);
+		 System.out.println("b" + b); 
+		if (b)
+			return "redirect:/registered"; // 수정 후 목록보기
+		else
+			return "error";
 	}
 }
