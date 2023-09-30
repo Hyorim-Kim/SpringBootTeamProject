a518fb7cc6f31f8713883988a3a988b45276697e
dfty1 <dfty1@192.168.219.102>
2023-09-30 13:00:17 +0900

    경도위도 넘기기

--- src/main/java/pack/controller/container/FormBean.java
+++ src/main/java/pack/controller/container/FormBean.java
@@ -10,5 +10,7 @@ import lombok.Data;
 public class FormBean {
     private int cont_price;
     private String cont_addr, cont_size, cont_image, cont_status, owner_name, owner_num, business_num, cont_no;
+    
+    private double cont_we, cont_kyung; // 재민 추가
 
 }
--- src/main/java/pack/model/container/ContainerDto.java
+++ src/main/java/pack/model/container/ContainerDto.java
@@ -4,15 +4,10 @@ import lombok.Data;
 
 @Data
 public class ContainerDto {
-
-	private String con_no,con_name,con_addr,we,kyung,con_area;
-	
 	// 민혁
-	
 	private int cont_no, cont_price;
 	private String cont_addr, cont_size, cont_image, owner_name, cont_status, business_num;
-	private String  cont_name, cont_we, cont_kyung,  owner_phone, owner_num, cont_area;
-
-
+	private String  cont_name,  owner_phone, owner_num, cont_area;
+	private double cont_we, cont_kyung; //재민 추가
 
 }
