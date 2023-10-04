5d1a9729886d21bed90b3271ce3ba1d40c515d73
kwangjin <kwangjin5468@naver.com>
2023-10-04 15:22:07 +0900

    10/4 광진 최종 병합 전 커밋

--- src/main/java/pack/controller/contact/ContactInsertController.java
+++ src/main/java/pack/controller/contact/ContactInsertController.java
@@ -13,26 +13,47 @@ import pack.model.contact.ContactDao;
 @Controller
 public class ContactInsertController {
 
-	@Autowired
-	private ContactDao contactDao;
-	
-	@GetMapping("insertcontact")
-	public String insert() {
-		return "../templates/contact/insert";
-	}
-	
-	@PostMapping("insertcontact")
-	public String insertSubmit(ContactBean bean, Model model) {
-		
-		LocalDateTime currentDateTime = LocalDateTime.now();
-		model.addAttribute("contact_date", currentDateTime);
-		
-		boolean b = contactDao.insertContact(bean);
-			
-		if(b) {
-			return "../templates/contact/contactmain"; // 추가 후 목록 보기
-		} else {
-			return "error";
-		}
-	}
+   @Autowired
+   private ContactDao contactDao;
+   
+   @GetMapping("insertcontactuser")
+   public String insertUser() {
+      return "../templates/contact/insert";
+   }
+   
+   @PostMapping("insertcontactuser")
+   public String insertSubmitUser(ContactBean bean, Model model) {
+      
+      LocalDateTime currentDateTime = LocalDateTime.now();
+      model.addAttribute("contact_date", currentDateTime);
+      
+      boolean b = contactDao.insertContact(bean);
+         
+      if(b) {
+         return "../templates/user/usermypage"; // 추가 후 목록 보기
+      } else {
+         return "error";
+      }
+   }
+   
+   @GetMapping("insertcontactowner")
+   public String insertOwner() {
+      return "../templates/contact/insert";
+   }
+   
+   @PostMapping("insertcontactowner")
+   public String insertSubmitOwner(ContactBean bean, Model model) {
+      
+      LocalDateTime currentDateTime = LocalDateTime.now();
+      model.addAttribute("contact_date", currentDateTime);
+      
+      boolean b = contactDao.insertContact(bean);
+         
+      if(b) {
+         return "../templates/owner/ownermain"; // 추가 후 목록 보기
+      } else {
+         return "error";
+      }
+   }
+   
 }
